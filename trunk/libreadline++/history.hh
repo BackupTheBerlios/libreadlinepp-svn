5067e4aea729ac40d7f4f7b6f79b0499e077a9a3
ka0ttic <ka0ttic@0890cc63-6509-0410-8bac-f4fd568f24b5>
2006-01-04 02:17:27 +0000

    Move inlines out of the main class definition.

--- trunk/libreadline++/history.hh
+++ trunk/libreadline++/history.hh
@@ -33,14 +33,9 @@
  */
 
 #include <string>
-#include <vector>
 #include <cstdlib>
 #include <libreadline++/exceptions.hh>
 #include <readline/history.h>
-
-#ifndef NELEMS
-# define NELEMS(x) (sizeof(x) / sizeof(x[0]))
-#endif
 
 namespace rl {
 
@@ -74,104 +69,65 @@ namespace rl {
             ~History() throw();
 
             /// Get path to history file.
-            inline const std::string& path() const { return _path; }
+            inline const std::string& path() const;
 
             /// Set path to history file.
-            inline void set_path(const std::string& path)
-            { _path.assign(path); }
+            inline void set_path(const std::string& path);
 
             /// Add line to history.
-            inline void add(const std::string& line)
-            {
-                add_history(line.c_str());
-                state();
-            }
+            inline void add(const std::string& line);
             
             /** Remove line from history.
              * @param offset offset of entry to remove.
              * @returns removed history entry.
              */
-            inline HistoryEntry *remove(int offset)
-            {
-                HistoryEntry *tmp = remove_history(offset);
-                state();
-                return tmp;
-            }
+            inline HistoryEntry *remove(int offset);
 
             /// Free history entry.
-            inline HistoryData free(HistoryEntry *entry)
-            { return free_history_entry(entry); }
+            inline HistoryData free(HistoryEntry *entry);
 
             /// Replace history entry.
-            inline HistoryEntry *replace(int which, const std::string& line,
-                    HistoryData data)
-            { return replace_history_entry(which, line.c_str(), data); }
+            inline HistoryEntry *replace(int offset, const std::string& line,
+                    HistoryData data);
 
             /// Get offset of current HistoryElement.
-            inline int offset() { return where_history(); }
+            inline int offset() const;
 
             /// Set current history offset.
-            inline bool set_offset(int offset)
-            {
-                const bool tmp = history_set_pos(offset);
-                state();
-                return tmp;
-            }
+            inline bool set_offset(int offset);
 
             /// Get current history entry.
-            inline HistoryEntry *current_entry() { return current_history(); }
+            inline HistoryEntry *current_entry() const;
 
             /// Get history entry at the specified offset.
-            inline HistoryEntry *entry_at_offset(int offset)
-            { return history_get(offset); }
+            inline HistoryEntry *entry_at_offset(int offset) const;
 
             /// Clear history.
-            inline void clear()
-            {
-                clear_history();
-                state();
-            }
+            inline void clear();
 
             /// Is the history stifled?
-            inline bool stifled() const { return history_is_stifled(); }
+            inline bool is_stifled() const;
 
             /** Stifle history.
              * @param max maximum number of entries to remember.
              */
-            inline void stifle(int max)
-            {
-                stifle_history(max);
-                state();
-            }
+            inline void stifle(int max);
 
             /** Unstifle history.
              * @returns previously-set maximum number of history entries (or a
              * negative value if history was not previously stifled).
              */
-            inline int unstifle()
-            {
-                int tmp = unstifle_history();
-                state();
-                return tmp;
-            }
+            inline int unstifle();
 
             /** Get current history state.
              * @returns a pointer to a HistoryState object.
              */
-            inline HistoryState *state()
-            {
-                _state = history_get_history_state();
-                return _state;
-            }
+            inline HistoryState *state() const;
 
             /** Set history state.
              * @param state pointer to a HistoryState object.
              */
-            inline void set_state(HistoryState *state)
-            {
-                history_set_history_state(state);
-                _state = state;
-            }
+            inline void set_state(HistoryState *state);
 
             /** Read history from disk.
              * @exception FileException
@@ -193,39 +149,157 @@ namespace rl {
             /** Get raw history list (NULL-terminated C array of HistoryEntry
              * pointers).
              */
-            inline HistoryList raw_list()
-            {
-                state();
-                return _state->entries;
-            }
+            inline HistoryList raw_list() const;
 
             /** Function template that returns a container of the specified type
              * filled with the contents of the current history list (as returned
              * by the raw_list() member function).  Note that the template
-             * argument must be explicitly specified.
+             * argument must be explicitly specified.  The implicit interface
+             * assumes that the template type has a constructor that accepts a
+             * range.
              */
             template <typename Container>
-            inline Container
-            list()
-            {
-                state();
-
-                if (_state->length == 0)
-                    return Container();
-
-                return Container(_state->entries,
-                                 _state->entries + _state->length);
-            }
+            inline Container list() const;
 
             /// Get current size of history list.
-            inline size_type size() { return history_length; }
+            inline size_type size() const;
 
         private:
             void _init() throw();
 
             std::string _path;
-            HistoryState *_state;
+            mutable HistoryState *_state;
     };
+
+    inline const std::string&
+    History::path() const
+    {
+        return _path;
+    }
+
+    inline void
+    History::set_path(const std::string& path)
+    {
+        _path.assign(path);
+    }
+
+    inline void
+    History::add(const std::string& line)
+    {
+        add_history(line.c_str());
+        state();
+    }
+
+    inline HistoryEntry *
+    History::remove(int offset)
+    {
+        HistoryEntry *tmp = remove_history(offset);
+        state();
+        return tmp;
+    }
+
+    inline HistoryData
+    History::free(HistoryEntry *entry)
+    {
+        return free_history_entry(entry);
+    }
+
+    inline HistoryEntry *
+    History::replace(int offset, const std::string& line, HistoryData data)
+    {
+        return replace_history_entry(offset, line.c_str(), data);
+    }
+
+    inline int
+    History::offset() const
+    {
+        return where_history();
+    }
+
+    inline bool
+    History::set_offset(int offset)
+    {
+        const bool tmp = history_set_pos(offset);
+        state();
+        return tmp;
+    }
+
+    inline HistoryEntry *
+    History::current_entry() const
+    {
+        return current_history();
+    }
+
+    inline HistoryEntry *
+    History::entry_at_offset(int offset) const
+    {
+        return history_get(offset);
+    }
+
+    inline void
+    History::clear()
+    {
+        clear_history();
+        state();
+    }
+
+    inline bool
+    History::is_stifled() const
+    {
+        return history_is_stifled();
+    }
+
+    inline void
+    History::stifle(int max)
+    {
+        stifle_history(max);
+        state();
+    }
+
+    inline int
+    History::unstifle()
+    {
+        const int tmp = unstifle_history();
+        state();
+        return tmp;
+    }
+
+    inline HistoryState *
+    History::state() const
+    {
+        _state = history_get_history_state();
+        return _state;
+    }
+
+    inline void
+    History::set_state(HistoryState *state)
+    {
+        history_set_history_state(state);
+        _state = state;
+    }
+
+    inline HistoryList
+    History::raw_list() const
+    {
+        state();
+        return _state->entries;
+    }
+
+    template <typename Container>
+    inline Container
+    History::list() const
+    {
+        state();
+        return (_state->length ?
+            Container(_state->entries, _state->entries + _state->length) :
+            Container());
+    }
+
+    inline History::size_type
+    History::size() const
+    {
+        return history_length;
+    }
 
 } // namespace rl
 
